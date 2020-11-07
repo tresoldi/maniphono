94d911b7ecb2b5746802c130b60dafb6063cc9c1
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-11-07 12:04:09 +0100

    Add missing tests

---
+++ tests/test_segment.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# pylint: disable=no-self-use
+
+"""
+test_segment
+============
+
+Tests for the Segment class of the `maniphono` package.
+"""
+
+# Import Python libraries
+import unittest
+from pathlib import Path
+
+# Import the library itself
+import maniphono
+
+
+class TestSegment(unittest.TestCase):
+    """
+    Suite of tests for segments.
+    """
+
+    def test_from_sounds(self):
+        # Define the sounds that will be used
+        snd0 = maniphono.Sound(maniphono.IPA, "w")
+        snd1 = maniphono.Sound(maniphono.IPA, "a")
+        snd2 = maniphono.Sound(maniphono.Tresoldi, "j")
+
+        # Test single-sound segment
+        seg1 = maniphono.Segment(snd1)
+        assert len(seg1) == 1
+        assert str(seg1) == "a"
+
+        # Test two-sound segment
+        seg2 = maniphono.Segment([snd1, snd2])
+        assert len(seg2) == 2
+        assert str(seg2) == "a+j"
+
+        # Test three-sound segment
+        seg3 = maniphono.Segment([snd0, snd1, snd2])
+        assert len(seg3) == 3
+        assert str(seg3) == "w+a+j"
+
+
+if __name__ == "__main__":
+    # Explicitly creating and running a test suite allows to profile it
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSegment)
+    unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ tests/test_sequence.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# pylint: disable=no-self-use
+
+"""
+test_sequence
+=============
+
+Tests for the Sequence class of the `maniphono` package.
+"""
+
+# Import Python libraries
+import unittest
+from pathlib import Path
+
+# Import the library itself
+import maniphono
+
+
+class TestSequence(unittest.TestCase):
+    """
+    Suite of tests for sequences.
+    """
+
+    def test_from_segments(self):
+        # Define the sounds that will be used
+        snd1 = maniphono.Sound(maniphono.IPA, "p")
+        snd2 = maniphono.Sound(maniphono.IPA, "a")
+        snd3 = maniphono.Sound(maniphono.IPA, "w")
+
+        # Define the segments that will be used
+        seg1 = maniphono.Segment(snd1)
+        seg2 = maniphono.Segment(snd2)
+        seg3 = maniphono.Segment([snd2, snd3])
+
+        # Test single-sound sequence
+        seq1 = maniphono.Sequence([seg1])
+        assert len(seq1) == 1
+        assert str(seq1) == "[p]"
+
+        # Test two-sound sequence (without diphthong)
+        seq2 = maniphono.Sequence([seg1, seg2])
+        assert len(seq2) == 2
+        assert str(seq2) == "[p a]"
+
+        # Test two-sound sequence (with diphthong)
+        seq3 = maniphono.Sequence([seg1, seg3])
+        assert len(seq3) == 2
+        assert str(seq3) == "[p a+w]"
+
+        # Test complex sequence
+        seq4 = maniphono.Sequence([seg1, seg2, seg1, seg3])
+        assert len(seq4) == 4
+        assert str(seq4) == "[p a p a+w]"
+
+
+if __name__ == "__main__":
+    # Explicitly creating and running a test suite allows to profile it
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSequence)
+    unittest.TextTestRunner(verbosity=2).run(suite)
