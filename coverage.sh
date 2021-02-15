cbad8a9c93d797412159aaef43cd72c2a36b8088
Tiago Tresoldi <tresoldi@shh.mpg.de>
2021-02-15 08:29:41 +0100

    Remove dead code

--- coverage.sh
+++
@@ -1,8 +0,0 @@
-env/bin/coverage run tests/test_phonomodel.py
-#env/bin/coverage run -a tests/test_regressors.py
-env/bin/coverage run -a tests/test_sound.py
-env/bin/coverage run -a tests/test_segment.py
-env/bin/coverage run -a tests/test_sequence.py
-env/bin/coverage run -a tests/test_utils.py
-env/bin/coverage html
-firefox htmlcov/index.html
