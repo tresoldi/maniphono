8668aa6ca1d403593299607a0ba018aed8c2a87a
Tiago Tresoldi <tresoldi@shh.mpg.de>
2021-03-27 09:58:58 +0100

    Try to fix package_data

--- setup.py
+++ setup.py
@@ -19,11 +19,13 @@ with open("requirements.txt", encoding="utf-8") as fp:
     install_requires = [dep.strip() for dep in fp.readlines()] + ["pytest"]
 
 # Build (recursive) list of resource files
-resource_files = []
-for directory in glob.glob("models/*/"):
-    files = glob.glob(directory + "*")
-    resource_files.append((directory, files))
-resource_files += glob.glob("distances/*")
+resource_files = {}
+resource_files["distances"] = "distances/*"
+resource_files["models"] = "models/*/*"
+#for directory in glob.glob("models/*/"):
+#    files = glob.glob(directory + "*")
+#    resource_files.append((directory, files))
+#resource_files += glob.glob("distances/*")
 
 # This call to setup() does all the work
 setup(
